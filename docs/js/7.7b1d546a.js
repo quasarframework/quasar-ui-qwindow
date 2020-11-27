9bb63f7dde014927a9fca04433f9ec5911cd24b8
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-11-27 11:48:12 -0700

    chore(docs): update demo

--- docs/js/7.7b1d546a.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[7],{a4ab:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[e._t("default")],2)},o=[],r={name:"ComponentProvide",provide:{myContext:"some injected data"},data(){return{}}},u=r,d=n("2877"),i=Object(d["a"])(u,a,o,!1,null,null,null);t["default"]=i.exports}}]);
