a9214ba92d2d9066cc117ccb12d7149b9a7955f8
Jeff <galbraith64@gmail.com>
2026-06-15 07:40:30 -0600

    fix(docs): propagate CodePen module import support

--- packages/docs/src/siteConfig/index.ts
+++ packages/docs/src/siteConfig/index.ts
@@ -73,6 +73,27 @@ export interface PrivacyConfig {
   link: string;
 }
 
+export interface CodepenGlobalPackage {
+  packageName: string;
+  globalName: string;
+}
+
+export interface CodepenModulePackage {
+  packageName: string;
+  importUrl: string;
+}
+
+export interface CodepenConfig {
+  head?: string;
+  cssExternal?: string[];
+  jsExternal?: string[];
+  jsPreProcessor?: string;
+  titleSuffix?: string;
+  jsSetup?: string;
+  globalPackages?: CodepenGlobalPackage[];
+  modulePackages?: CodepenModulePackage[];
+}
+
 export interface SiteConfig {
   lang: string;
   title: string;
@@ -82,6 +103,7 @@ export interface SiteConfig {
   copyright: CopyrightConfig;
   githubEditRootSrc: string;
   githubSourceRootSrc?: string;
+  codepen?: CodepenConfig;
   license: LicenseConfig;
   privacy: PrivacyConfig;
   logoConfig: LogoConfig;
