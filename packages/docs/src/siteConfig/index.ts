904ff14ab102026b88c6835dbde14cf2c27806c3
Jeff <galbraith64@gmail.com>
2026-06-25 09:47:31 -0600

    fix: support pathless sidebar groups

--- packages/docs/src/siteConfig/index.ts
+++ packages/docs/src/siteConfig/index.ts
@@ -157,6 +157,10 @@ export interface SiteConfig {
 }
 
 function getSidebarPath(item: SiteMenuItem): string {
+  if (item.path === '') {
+    return ''
+  }
+
   if (item.external === true) {
     return item.path ?? slugify(item.name)
   }
