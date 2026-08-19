dd2e89ae79a24e08f4b6aa86fe554c4433c2cc6b
Jeff <galbraith64@gmail.com>
2026-08-19 07:01:05 -0600

    fix(app-extension): exclude UI package from Vite optimization
    Refs quasarframework/quasar#14122

--- packages/app-extension/src/index.ts
+++ packages/app-extension/src/index.ts
@@ -11,6 +11,12 @@ export default defineIndexScript((api) => {
   api.compatibleWith('quasar', '^2.0.0')
   api.compatibleWith('@quasar/app-vite', '>=3.0.0')
 
+  api.extendViteConf(() => ({
+    optimizeDeps: {
+      exclude: ['@quasar/quasar-ui-qwindow'],
+    },
+  }))
+
   api.registerDescribeApi('QWindow', '~@quasar/quasar-ui-qwindow/dist/api/QWindow.json')
 
   api.extendQuasarConf(() => ({
